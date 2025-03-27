d3c3b5c744713d69b5dda32626a68ebd22d24ead
DanyQuiroz2 <dqc220618@gmail.com>
2025-03-27 14:14:20 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,23 @@
+//Danila Quiroz Cruz
+ejercicio1
+programa para validar si si a es mayor a b.*/ 
+
+
+# include <stdio.h>
+
+void main ()
+{
+    int a, b;
+    
+    printf ("ingresa el valor de a");
+    scanf ("%i",&a);
+
+    printf ("ingresa el valor de b");
+    scanf ("%i",&b);
+
+    if a>b 
+
+    printf ("a es mayor que b /n");
+    }
+printf ("a es mayor que b /n");
+}
